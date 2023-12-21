ac9dbf765482ab28b1eb5fee936d1defaa504328
hRJ06 <hindolroy.2306@gmail.com>
2023-12-22 03:01:48 +0530

    Updated Visulization for Plotly

--- Visualization/app.py
+++ Visualization/app.py
@@ -1,10 +1,49 @@
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect, send_file, jsonify
+import os
 import pandas as pd
-from io import BytesIO
-import plotly.express as px
-import seaborn as sns
-import matplotlib.pyplot as plt
+from pandas.api.types import is_numeric_dtype
+import plotly
+import plotly.graph_objects as go
+import json
+
+def plotly_Scatter(column1, column2, column1_name, column2_name, mode='markers', marker_size=5):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=column1, y=column2, mode=mode, marker_size=int(marker_size)))
+    fig.update_layout(
+        xaxis_title=column1_name, yaxis_title=column2_name
+    )
+    fig.show()
+
+def plotly_Bar(column1, column2, column1_name, column2_name):
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=column1, y=column2))
+    fig.update_layout(
+        xaxis_title=column1_name, yaxis_title=column2_name
+    )
+    fig.show()
+
+def plotly_Histogram(column1, column1_name):
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(x=column1))
+    fig.update_layout(
+        xaxis_title=column1_name,
+    )
+    fig.show()
+
+def plotly_Scatter3D(column1, column2, column3, column1_name, column2_name, column3_name, mode='markers'):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter3d(x=column1, y=column2, z=column3, mode=mode))
+    fig.update_layout(
+        scene=dict(
+            xaxis_title=column1_name, yaxis_title=column2_name, zaxis_title=column3_name
+        )
+    )
+    fig.show()
+
 app = Flask(__name__)
+FILE_NAME = ''
+TABLES = []
+df = None
 
 @app.route('/')
 def index():
@@ -12,62 +51,58 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    global FILE_NAME, TABLES, df
     if 'file' not in request.files:
         return redirect(request.url)
     file = request.files['file']
-    if file.filename == '':
-        return redirect(request.url)
     if file:
-        if file.filename.endswith('.csv'):
-            df = pd.read_csv(file)
-            return render_template('tables.html', tables=list(df.columns))
-    return 'Invalid file format. Please upload a CSV file.'
+        if not os.path.exists('uploads'):
+            os.makedirs('uploads')
+        filename = os.path.join('uploads', file.filename)
+        file.save(filename)
+        FILE_NAME = filename
+    df = pd.read_csv(FILE_NAME)
+    for i in df.columns:
+        if is_numeric_dtype(df[i]):
+            TABLES.append(i)
+    return render_template('index.html', tables=TABLES)
 
-@app.route('/process', methods=['POST'])
-def process_data():
-    table_name = request.form['table']
-    option = request.form['option']
-    if option == 'create_table':
-        result_df = pd.DataFrame(columns=[table_name, 'Column1', 'Column2'])
-    elif option == 'fetch_data':
-        result_df = pd.DataFrame(columns=[table_name, 'Column1', 'Column2'])
-    elif option == 'fetch_column_data':
-        column_name = request.form['column']
-        result_df = pd.DataFrame(columns=[table_name, column_name])
-    elif option == 'bar_chart':
-        fig = px.bar(result_df, x=table_name, y=column_name, title=f'Bar Chart for {column_name}')
-        output_file = BytesIO()
-        fig.write_image(output_file, format='png')
-        output_file.seek(0)
-        return send_file(output_file, as_attachment=True, download_name=f'{table_name}_{column_name}_bar_chart.png')
-    elif option == 'histogram':
-        fig = px.histogram(result_df, x=column_name, title=f'Histogram for {column_name}')
-        output_file = BytesIO()
-        fig.write_image(output_file, format='png')
-        output_file.seek(0)
-        return send_file(output_file, as_attachment=True, download_name=f'{table_name}_{column_name}_histogram.png')
-    elif option == 'heatmap':
-        # Assuming you have a numeric dataset for the heatmap
-        sns.set()
-        plt.figure(figsize=(10, 8))
-        sns.heatmap(result_df.corr(), annot=True, cmap='coolwarm', fmt=".2f")
-        plt.title(f'Heatmap for {table_name}')
-        output_file = BytesIO()
-        plt.savefig(output_file, format='png')
-        output_file.seek(0)
-        return send_file(output_file, as_attachment=True, download_name=f'{table_name}_heatmap.png')
-    elif option in ['mean', 'count', 'average']:
-        column_name = request.form['column']
-        if option == 'mean':
-            result_value = result_df[column_name].mean()
-        elif option == 'count':
-            result_value = result_df[column_name].count()
-        elif option == 'average':
-            result_value = result_df[column_name].sum() / result_df[column_name].count()
-        return f"{option.capitalize()} for {column_name}: {result_value}"
-    output_file = BytesIO()
-    result_df.to_csv(output_file, index=False)
-    output_file.seek(0)
-    return send_file(output_file, as_attachment=True, download_name=f'{table_name}_output.csv')
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = json.loads(request.data.decode('utf-8'))
+    plotLibrary = data["plotLibrary"]
+    plotType = data["plotType"]
+    mode = data.get("scatterMode", "")  
+    size = data.get("markerSize", "")   
+    tables = data["selectedTables"]
+    print(plotLibrary, plotType, mode, size, tables)
+    
+    if plotLibrary == 'plotly':
+        if plotType == "scatter":
+            x = df[tables[0]]
+            y = df[tables[1]]
+            mode = mode
+            size = size
+            plotly_Scatter(x, y, tables[0], tables[1], mode, size)
+            return
+        elif plotType == "bar":
+            x = df[tables[0]]
+            y = df[tables[1]]
+            plotly_Bar(x, y, tables[0], tables[1])
+            return 
+        elif plotType == "histogram":
+            x = df[tables[0]]
+            plotly_Bar(x, tables[0])
+            return
+        else:
+            x = df[tables[0]]
+            y = df[tables[1]]
+            z = df[tables[2]]
+            mode = mode
+            plotly_Scatter3D(x, y, z, tables[0], tables[1], tables[2], mode)
+            return
+    else:
+        return
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port='4000',debug=True)
+    app.run()
